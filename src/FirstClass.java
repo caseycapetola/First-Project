342814c2bb614ae75dd73728ce8a8d8bf3dd01b1
Casey <Casey@DESKTOP-61H6QIQ.fios-router.home>
2018-09-23 21:55:53 -0400

    Added new line of text

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -4,7 +4,8 @@ public class FirstClass
 
 	public static void main(String[] args) 
 	{
-		System.out.print("hello, everyone");
+		System.out.println("hello, everyone");
+		System.out.print("this is my first repository");
 
 	}
 
